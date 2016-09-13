4891be26fde7ade11126b8b6c6d6245a1cc0172d
Liugenggeng <m14743163347@163.com>
2016-09-13 13:32:38 +0800

    title设置

--- wechat/js/app.js
+++ wechat/js/app.js
@@ -1,7 +1,12 @@
 /**
  * Created by Administrator on 2016/9/12.
  */
-var app = angular.module('xzm',['ui.router']);
+var app = angular.module('xzm',['ui.router'])
+    .run(['$location','$rootScope',function($location, $rootScope){
+        $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+            $rootScope.title = toState.title
+        });
+    }]);
 app.config(function ($stateProvider, $urlRouterProvider)  {
     $urlRouterProvider.when("", "/releaseList");
     $urlRouterProvider.otherwise('/releaseList');
@@ -9,21 +14,25 @@ app.config(function ($stateProvider, $urlRouterProvider)  {
         .state("releaseList", {
             url: "/releaseList",
             templateUrl: 'tpls/releaseList.html',
-            title:"发布列表"
-        })
-        .state("myRelease", {
-            url: "/myRelease",
-            templateUrl: 'tpls/myRelease.html',
-            title:"我的发布"
+            title:"发布列表"/*,
+            controller:"releaseListCtrl"*/
         })
         .state("release", {
             url: "/release",
             templateUrl: 'tpls/release.html',
-            title:"发布"
+            title:"发布"/*,
+            controller:"releaseCtrl"*/
+        })
+        .state("myRelease", {
+            url: "/myRelease",
+            templateUrl: 'tpls/myRelease.html',
+            title:"我的发布"/*,
+            controller:"myReleaseCtrl"*/
         })
         .state("itemDetails", {
             url: "/itemDetails",
             templateUrl: 'tpls/itemDetails.html',
-            title:"物品详情"
+            title:"物品详情"/*,
+            controller:"itemDetailsCtrl"*/
         })
 })
--- wechat/js/controller.js
+++ wechat/js/controller.js
@@ -1,3 +1,12 @@
 /**
  * Created by Administrator on 2016/9/12.
  */
+/****************************/
+/***不同页面设置不同的title***/
+/****************************/
+angular.module('xzm', [ ] )
+.run(['$location','$rootScope',function($location, $rootScope){
+    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        $rootScope.title = toState.title
+    });
+}]);
